4d1cb5d34515f0783ad16588b6bf63f3f732f5bf
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-11-17 09:29:35 +0100

    feat(reactivity-debugging): setup of debugging

---
+++ fous/reactivity-debugging/src/components/CounterButton.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+import { onRenderTracked, onRenderTriggered } from 'vue'
+
+onRenderTracked((event) => {
+  debugger
+})
+
+onRenderTriggered((event) => {
+  debugger
+})
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+const plusOne = computed(() => count.value + 1, {
+  onTrack(e) {
+    // triggered when count.value is tracked as a dependency
+    debugger
+  },
+  onTrigger(e) {
+    // triggered when count.value is mutated
+    debugger
+  },
+})
+</script>
+
+<template>
+  <button @click="increment">
+    {{ count }}
+  </button>
+
+  {{ plusOne }}
+</template>
